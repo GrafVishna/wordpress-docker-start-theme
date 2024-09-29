d218cfeb86addea8432fab9dbdadc82ea4cc8a02
GrafVishna <grafvishna4@gmail.com>
2024-09-29 20:21:58 +0300

    pl

---
+++ plugin/core.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Plugin-Core
+Plugin URI: http://core.com
+Description: Plugin.
+Version: 1.0
+Author: Author
+Author URI: http://author.com
+License: GPLv2 or later
+Text Domain: plugin-pore
+*/
